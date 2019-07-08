9c10530a0b1dd9c4aed7a767c61bcb9d86088af8
Dev Temp <devtemp@HC-iMac-IT.local>
2019-07-08 15:28:21 -0400

    Signin form and custom form and signin button

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.componet';
+
+export default class SignIn extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        const {name, value} = event.target;
+
+        this.setState({[name]: value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        this.setState({email:'', password:''});
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} required label="Email"/>
+                    <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} required label="Password"/>
+                    <CustomButton type="submit"> Sign In </CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
--- src/views/sign-in/sign-in.component.jsx
+++ src/views/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './sign-in.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
 
 export default function SignInPage() {
     return (
         <div className="sign-in-and-sign-up">
-            SIGN IN
+            <SignIn/>
         </div>
     )
 }
