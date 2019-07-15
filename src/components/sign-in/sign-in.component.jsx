fce0d896f8693ac2e550620ddf7dfbd8b41ef89a
Dev Temp <devtemp@HC-iMac-IT.local>
2019-07-15 16:25:44 -0400

    setup sign-in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.componet';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 export default class SignIn extends Component {
     constructor(props){
@@ -24,10 +24,18 @@ export default class SignIn extends Component {
         this.setState({[name]: value});
     }
 
-    handleSubmit(event){
+    async handleSubmit(event){
         event.preventDefault();
 
-        this.setState({email:'', password:''});
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''});
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
     render() {
