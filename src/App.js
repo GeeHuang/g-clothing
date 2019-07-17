ef672d0018f60c146e33db46fb7e6111e824cc44
Dev Temp <devtemp@HC-iMac-IT.local>
2019-07-17 16:14:20 -0400

    add user types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import './App.css';
@@ -48,15 +48,19 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage}/>
           <Route exact path='/contact' component={ContactPage}/>
-          <Route exact path='/sign-in-sign-up' component={SignInSignUpPage}/>
+          <Route exact path='/sign-in-sign-up' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInSignUpPage/>) }/>
         </Switch>
       </div>
     );
   }
 } 
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
